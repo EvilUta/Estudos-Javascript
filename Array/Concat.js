959d89ff539843e4098137bdb8566a38b80ff16d
Renan Gustavo <Renangustavo.lemes@gmail.com>
2025-09-11 16:07:49 -0300

    Aula de concat

---
+++ Array/Concat.js
@@ -0,0 +1,6 @@
+const filhas = ["Ualeskah", "Cibanela"]
+const filhos = ["Uoxiton", "Uesclei"]
+const todos = filhos.concat(filhos)
+console.log(todos, filhas, filhas)
+
+console.log(["a","b"].concat([1,2],[2,4], 5 , [[6,7]]))
