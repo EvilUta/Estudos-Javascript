b21243cdc99d890b1f75b8861a747572ad1c49b8
Renan Gustavo <Renangustavo.lemes@gmail.com>
2025-10-02 14:04:24 -0300

    Aulas de css

---
+++ css/css/js/tag.js
@@ -0,0 +1,13 @@
+document.querySelectorAll('.tag').forEach(box => {
+    const tagName = box.tagName.toLowerCase()
+
+    box.style.borderColor = "#616161"
+
+    if (!box.classList.contains('nolabel')) {
+        const label = document.createElement('label')
+        label.style.backgroundColor = '#616161'
+        label.innerHTML = tagName
+        // Melhor usar firstChild se quer inserir no começo
+        box.insertBefore(label, box.firstChild)
+    }
+})
