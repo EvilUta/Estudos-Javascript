aacc52147a767a600f6bc51897e39a8760d26632
Renan Gustavo <Renangustavo.lemes@gmail.com>
2025-10-08 13:49:45 -0300

    Aula CSS Grid #03 — layout com grid-template-areas Aprendizado sobre: - Uso da propriedade grid-template-areas para estruturar layouts semânticos. - Definição de áreas nomeadas no grid (cabecalho, navegacao, conteudo e rodape). - Configuração de colunas e linhas com grid-template-columns e grid-template-rows. - Aplicação de min-height: 100vh para ocupar toda a altura da tela. - Centralização dos conteúdos em cada área usando display: flex e justify-content: center. - Espaçamento entre label e conteúdo com margin-right. Mudanças: - Mudança no tag.js - Estilização da pagina - Cores e bordas arredondadas

--- css/css/js/tag.js
+++ css/css/js/tag.js
@@ -1,13 +1,36 @@
 document.querySelectorAll('.tag').forEach(box => {
     const tagName = box.tagName.toLowerCase()
-
-    box.style.borderColor = "#616161"
+    box.style.borderColor = "#0e85fdc4"
+    box.style.backgroundColor = "rgba(250, 250, 250, 0.91)"
+    box.style.borderRadius = "12px"
 
     if (!box.classList.contains('nolabel')) {
         const label = document.createElement('label')
-        label.style.backgroundColor = '#616161'
         label.innerHTML = tagName
-        // Melhor usar firstChild se quer inserir no começo
+        label.style.color = "#fff"
+        label.style.padding = "6px 12px"
+        label.style.fontSize = "1rem"
+        label.style.borderRadius = "12px"
+
+        // 🎨 Cores diferentes para cada tipo de tag
+        switch (tagName) {
+            case 'header':
+                label.style.backgroundColor = '#f94144' // vermelho
+                break
+            case 'nav':
+                label.style.backgroundColor = '#f3722c' // laranja
+                break
+            case 'main':
+                label.style.backgroundColor = '#43aa8b' // verde
+                break
+            case 'footer':
+                label.style.backgroundColor = '#577590' // azul
+                break
+            default:
+                label.style.backgroundColor = '#999' // cor padrão
+        }
+
         box.insertBefore(label, box.firstChild)
     }
 })
+
