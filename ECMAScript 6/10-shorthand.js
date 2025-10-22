f0a9a6d89778831bae49e425f74b2c4a91fca229
Renan Gustavo <Renangustavo.lemes@gmail.com>
2025-10-22 18:25:00 -0300

    Atualizacao da ES6 que da versoes curtas e faceis de function e objects

---
+++ ECMAScript 6/10-shorthand.js
@@ -0,0 +1,28 @@
+
+    let firstName = 'Renan';
+    let surname = 'Gustavo';
+    let age = 21;
+
+    // const person = {
+    //     firstName: firstName,
+    //     surname: surname,
+    //     age: age
+    // }
+
+// Melhoria
+    const person = {
+        firstName,
+        surname,
+        age,
+    hello(){
+    console.log('oiiiiii')
+        }
+    }
+
+console.log(person);
+person.hello();
+
+// da na mesma e muito mais facil e curto
+// funciona na mesma forma com function
+
+
